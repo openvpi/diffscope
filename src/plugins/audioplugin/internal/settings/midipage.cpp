096223de40eebee0a36672ee59c029ccf81ada10
CrSjimo <crs_16423@outlook.com>
2024-04-29 01:43:11 +0800

    fix midi page

--- src/plugins/audioplugin/internal/settings/midipage.cpp
+++ src/plugins/audioplugin/internal/settings/midipage.cpp
@@ -1,3 +1,19 @@
 #include "midipage.h"
 
-namespace Audio {}
+namespace Audio {
+    MIDIPage::MIDIPage(QObject *parent) : Core::ISettingPage("audio.MIDI", parent) {
+    }
+    MIDIPage::~MIDIPage() {
+    }
+    QString MIDIPage::sortKeyword() const {
+        return ISettingPage::sortKeyword();
+    }
+    QWidget *MIDIPage::widget() {
+        return nullptr;
+    }
+    bool MIDIPage::accept() {
+        return false;
+    }
+    void MIDIPage::finish() {
+    }
+}
